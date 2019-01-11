649719c2d00dbe063b3d3c0c17859aa9dc37d950
Renxun Yang <yangrenxun@outlook.com>
2019-01-11 17:48:18 +0800

    add mediatr

---
+++ GreatBear/GreatBear.MediatR/Event.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GreatBear.MediatR
+{
+    public class Event : INotification
+    {
+    }
+
+    public class Handle : INotificationHandler<Event>
+    {
+        Task INotificationHandler<Event>.Handle(Event notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Handle2 : INotificationHandler<Event>
+    {
+        Task INotificationHandler<Event>.Handle(Event notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
